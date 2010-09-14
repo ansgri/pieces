57245576c52e8c97d43647d00df14934f37d3507
ansgri <ansgri@gmail.com>
2010-09-14 22:51:50 +0400

    overfitting rms with exclusions

--- overfitting.m
+++ overfitting.m
@@ -1,7 +1,10 @@
-coeffs = [1 1 4 -5 -3 5 1]
-N_DATA_POINTS = 30
-x = sort(rand([N_DATA_POINTS 1]) * 2)
-y = polyval(coeffs, x)
+COEFFS = [1 1 4 -5 -3 5 1]
+DATA_POINTS = 30
+POWERS = 1:15
+EXCLUSIONS = 10
+
+x = sort(rand([DATA_POINTS 1]) * 2)
+y = polyval(COEFFS, x)
 y += randn([length(y) 1])
 plot(x, y)
 
@@ -15,6 +18,24 @@ function r = rmsForPower(x, y, pow)
   r = mean((yFit - y) .^ 2);
 end
 
-pows = 1:15;
+pows = POWERS;
 rmss = arrayfun(@(p)rmsForPower(x, y, p), pows);
-plot(pows, rmss);
+
+randPerm = randperm(length(x))
+exIndices = sort(randPerm(1:EXCLUSIONS))
+restIndices = sort(randPerm((EXCLUSIONS + 1):length(randPerm)))
+
+function r = rmsForPowerEx(x, y, pow, exIndices, restIndices)
+  xRest = x(restIndices)
+  yRest = y(restIndices)
+
+  mm = powerMat(xRest, pow:-1:0);
+  yFit = polyval(mm \ yRest, x(exIndices));
+  r = mean((yFit - y(exIndices)) .^ 2);
+end
+
+exNum = 10
+rmssEx = arrayfun(@(p)rmsForPowerEx(x, y, p, exIndices, restIndices), pows);
+
+% the resulting plot
+semilogy(pows, [rmssEx; rmss]);
